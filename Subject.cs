c3284ede67a0b74269bf68edbf76ceff4b4d6992
jeeban <jeebanjyoti193@gmail.com>
2023-08-02 20:37:40 +0530

    first commit

---
+++ Subject.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Phase1Section5._6
+{
+    public class Subject
+    {
+        private string name;
+        private string shortName;
+
+        public string Name
+        {
+            get { return name; }
+            set { this.name = value; }
+        }
+
+        public string ShortName
+        {
+            get { return shortName; }
+            set { shortName = value; }
+        }
+
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Phase1Section5._6
+{
+    public class Teacher
+    {
+        private string name;
+        private string address;
+        private string contactAddress;
+        private DateTime dateOfJoining;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string ContactAddress
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public DateTime DateOfJoining
+        {
+            get { return dateOfJoining; }
+            set { dateOfJoining = value; }
+        }
+    }
+}
